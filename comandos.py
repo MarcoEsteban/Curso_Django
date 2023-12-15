98e02215d4743d38319c1d229dd9559bb103d5ed
MarcoCampos <marcoscampomc43@gmail.com>
2023-12-14 20:08:54 -0400

    first commit

---
+++ comandos.py
@@ -0,0 +1,27 @@
+# Siempre trabajar con ambientes Virtuales:
+
+################################################################################
+Instalamos un Paquete para trabajar con AmbienteVirtual ejm 'virtualenv'
+################################################################################
+    
+    # Paso 1: Nos ubicamos en la carpeta donde vamos a trabajar: 
+    cd /home/marco/Desktop/SoloPython/Curso_Django
+
+    # Paso 2: Instalar paquete Virtual:
+    sudo pip install virtualenv
+
+    # Paso 3: Creamos nuestro ambiente virtual:
+    virtualenv name_ambiente
+
+    # Paso 4: Activamos el Ambiente virtual:
+    source env/bin/activate #Linux 
+    cd env/Scripts>activate #Windowns
+
+################################################################################
+Integrar github con el ambiente virtual:
+################################################################################
+
+    # Paso 1: Conectar nuestro proyecto Virtual con github:
+    
+
+
